3b671ce401aa4adf2110ecd8edf437150f6afb47
pony13500815917 <13500815917@163.com>
2022-10-17 14:04:07 +0800

    ✨ add edge method

--- src/pages/api/edge.ts
+++ src/pages/api/edge.ts
@@ -1,11 +1,54 @@
 import { NextRequest } from 'next/server';
 
-export default (req: NextRequest) =>
-  new Response(JSON.stringify({ name: 'pony', age: 28, ip: req.ip }), {
-    headers: {
-      'content-type': 'application/json;charset=UTF-8'
+export default async (req: NextRequest) => {
+  try {
+    switch (req.method) {
+      case 'GET': {
+        const { searchParams } = new URL(req.url);
+        const age = searchParams.get('age');
+
+        return new Response(
+          JSON.stringify({
+            name: 'pony',
+            age,
+            ip: req.ip,
+            method: req.method
+          }),
+          {
+            headers: {
+              'content-type': 'application/json;charset=UTF-8'
+            }
+          }
+        );
+      }
+      case 'POST': {
+        const jsonData = await req.json();
+
+        return new Response(
+          JSON.stringify({
+            name: 'pony',
+            age: jsonData.age,
+            ip: req.ip,
+            method: req.method
+          }),
+          {
+            headers: {
+              'content-type': 'application/json;charset=UTF-8'
+            }
+          }
+        );
+      }
+      default:
+        return new Response(null, {
+          status: 404
+        });
     }
-  });
+  } catch (err) {
+    return new Response(null, {
+      status: 500
+    });
+  }
+};
 
 export const config = {
   runtime: 'experimental-edge'
