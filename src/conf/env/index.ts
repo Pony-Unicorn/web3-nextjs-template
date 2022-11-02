1853e0128d09027ea9d46f8c5fce107f477a6723
pony13500815917 <13500815917@163.com>
2022-11-02 11:49:30 +0800

    🐛 Fix Defaults

--- src/conf/env/index.ts
+++ src/conf/env/index.ts
@@ -7,7 +7,7 @@ import production from './production';
 // 2、Publicable configuration writes development or production file
 // You can configure different environment variables
 // NEXT_PUBLIC_APP_MODE production, preview, or development.
-const MODE = process.env.NEXT_PUBLIC_APP_MODE;
+const MODE = process.env.NEXT_PUBLIC_APP_MODE || 'development';
 
 const CONF_ENV = MODE === 'production' ? production : development;
 
